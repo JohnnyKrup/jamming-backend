45fa0c858b199d8cc72d991dd50bdc586d5c0512
graziano <graziano.laveder@gamil.com>
2019-04-22 12:44:03 +0200

    Documentation

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ let querystring = require('querystring')
 
 let app = express()
 
-let redirect_uri = 
-  process.env.REDIRECT_URI || 
-  'http://localhost:8888/callback'
+// hardcoded localhost RedirectURI
+let redirect_uri = process.env.REDIRECT_URI || 'http://localhost:8888/callback'
 
 app.get('/login', function(req, res) {
   res.redirect('https://accounts.spotify.com/authorize?' +
@@ -36,11 +35,12 @@ app.get('/callback', function(req, res) {
   }
   request.post(authOptions, function(error, response, body) {
     var access_token = body.access_token
+    // hardcoded react localhost URI
     let uri = process.env.FRONTEND_URI || 'http://localhost:3000'
     res.redirect(uri + '?access_token=' + access_token)
   })
 })
 
 let port = process.env.PORT || 8888
-console.log(`Listening on port ${port}. Go /login to initiate authentication flow.`)
+console.log(`Listening on port ${port}. Go to /login to initiate authentication flow.`)
 app.listen(port)
