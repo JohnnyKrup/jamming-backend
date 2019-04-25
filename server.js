2ac1d9499ec1b8e8350a38105c1da7375caf33a1
graziano <graziano.laveder@gamil.com>
2019-04-25 21:24:00 +0200

    Added more scope to create playlists

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/login', function(req, res) {
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email',
+      scope: 'user-library-modify playlist-modify-private user-read-private user-read-email playlist-modify-public playlist-read-private',
       redirect_uri
     }))
 })
